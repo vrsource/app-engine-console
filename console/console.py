593e2421a95c3f4ace725d3e7aff3f5ef5343dbc
Jason Smith <jhs@proven-corporation.com>
2008-08-25 16:14:50 +0700

    Get a rough InteractiveInterpreter working

--- console/console.py
+++ console/console.py
@@ -15,24 +15,56 @@
 # along with App Engine Console; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+import sys
 import cgi
+import code
 import logging
+import StringIO
 import simplejson
+
+# For some reason, sys.modules wants __builtin__ or else the InteractiveInterpreter
+# will throw exceptions when evaluating expressions.
+import __builtin__
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+class AppEngineInterpreter(code.InteractiveInterpreter):
+    """An interactive interpreter suitable for running within the App Engine."""
+    def __init__(self, *args, **kw):
+        code.InteractiveInterpreter.__init__(self, *args, **kw)
+
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+        self.buf    = StringIO.StringIO()
+        
+    def runsource(self, *args, **kw):
+        logging.debug('source: %s' % args[0])
+        self.buf.truncate(0)
+
+        sys.stdout, sys.stderr = self.buf, self.buf
+        code.InteractiveInterpreter.runsource(self, *args, **kw)
+        sys.stdout, sys.stderr = self.stdout, self.stderr
+
+        self.buf.seek(0)
+        logging.debug('result: %s' % self.buf.read())
+        self.buf.seek(0)
+
 class Console(webapp.RequestHandler):
+    def __init__(self):
+        self.engine = AppEngineInterpreter(globals())
+
     def write(self, *args, **kw):
         self.response.out.write(*args, **kw)
 
     def get(self):
         code = self.request.get('code')
-        result = code
+        result = self.engine.runsource(code)
         response = {
             'in' : code,
-            'out': result,
+            'out': self.engine.buf.read().strip(),
+            'result': result,
         }
 
         self.response.headers['Content-Type'] = 'application/x-javascript'
