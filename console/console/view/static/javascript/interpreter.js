371c6ed973f42a04fd3cfbb47904ca9eba3a95cd
Jason Smith <jhs@proven-corporation.com>
2008-08-29 19:46:50 +0700

    Remove unused code

--- console/console/view/static/javascript/interpreter.js
+++ console/console/view/static/javascript/interpreter.js
@@ -24,9 +24,6 @@ InterpreterManager.prototype.initialize = function () {
         }
     }
 
-    window.ps1 = '>>> ';
-    window.ps2 = '... ';
-
     window.help = this.help;
     this.help.NAME = 'type help(func) for help on a MochiKit function';
 
@@ -37,53 +34,6 @@ InterpreterManager.prototype.initialize = function () {
         this.setTeamwork();
     }
 };
-
-InterpreterManager.prototype.submit = function (event) {
-    if (this.blockingOn) {
-        try {
-            this.blockingOn.cancel();
-        } catch (e) {
-            this.showError(e);
-        }
-        this.blockingOn = null;
-    }
-    this.doSubmit();
-    this.doScroll();
-    event.stop();
-};
-
-InterpreterManager.prototype.help = function (fn) {
-    if (fn && fn.NAME) {
-        fn = fn.NAME;
-    }
-    if (typeof(fn) != "string" || fn.length == 0) {
-        writeln("help(func) on any MochiKit function for help");
-        return;
-    }
-    var comps = fn.split('.');
-    var base = comps.splice(0, 2);
-    var shortfn = comps.join('.');
-    var url = '../../doc/html/' + base.join('/') + '.html';
-    var d = doXHR(url, {mimeType: 'text/xml'});
-    d.addCallback(function (req) {
-        var els = getElementsByTagAndClassName(
-            'a', 'mochidef', req.responseXML);
-        var match = '#fn-' + shortfn.toLowerCase();
-        for (var i = 0; i < els.length; i++) {
-            var elem = els[i];
-            var href = elem.href;
-            var idx = href.indexOf('#');
-            if (idx != -1 && href.substring(idx) == match) {
-                writeln(A({href: url + match, target: '_blank'},
-                    scrapeText(elem)));
-                return;
-            }
-        }
-        writeln('documentation for ' + fn + ' not found');
-    });
-    blockOn(d);
-};
-
 
 InterpreterManager.prototype.doScroll = function () {
     var p = getElement("interpreter_output").lastChild;
@@ -124,61 +74,6 @@ InterpreterManager.prototype.moveHistory = function (dir) {
     this.historyPos += dir;
     elem.value = this.history[this.historyPos];
 }
-
-InterpreterManager.prototype.runMultipleLines = function (text) {
-    var lines = rstrip(text).replace("\r\n", "\n").split(/\n/);
-    appendChildNodes("interpreter_output",
-        SPAN({"class": "code"}, window.ps1, izip(lines, imap(BR, cycle([null]))))
-    );
-    this.runCode(text);
-}
-
-InterpreterManager.prototype.blockOn = function (d) {
-    var node = SPAN({"class": "banner"}, "blocking on " + repr(d) + "...");
-    this.blockingOn = d;
-    appendChildNodes("interpreter_output", node);
-    this.doScroll();
-    d.addBoth(function (res) {
-        swapDOM(node);
-        this.blockingOn = null;
-        if (res instanceof CancelledError) {
-            window.writeln(SPAN({"class": "error"}, repr(d) + " cancelled!"));
-            return undefined;
-        }
-        return res;
-    });
-    d.addCallbacks(this.showResult, this.showError);
-};
-
-EvalFunctions = {
-    evalWith: function () {
-        with (arguments[1] || window) { return eval(arguments[0]); };
-    },
-    evalCall: function () {
-        return eval.call(arguments[1] || window, arguments[0]);
-    },
-    choose: function () {
-        var ns = {__test__: this};
-        var e;
-        try {
-            if (this.evalWith("return __test__", ns) === this) {
-                return this.evalWith;
-            }
-        } catch (e) {
-            // pass
-        }
-        try {
-            if (this.evalCall("return __test__", ns) === this) {
-                return this.evalCall;
-            }
-        } catch (e) {
-            // pass
-        }
-        return undefined;
-    }
-};
-        
-InterpreterManager.prototype.doEval = EvalFunctions.choose();
 
 InterpreterManager.prototype.doSubmit = function () {
     var elem = getElement("interpreter_text");
@@ -221,27 +116,6 @@ InterpreterManager.prototype.doSubmit = function () {
     }
 };
 
-InterpreterManager.prototype.showResult = function (res) {
-    if (typeof(res) != "undefined") {
-        window._ = res;
-    }
-    if (typeof(res) != "undefined") {
-        if(getElement('setting_highlight').value == 'Highlighting') {
-            var formatted = DIV({'class':'pygments data'});
-            formatted.innerHTML = res;
-            appendChildNodes('interpreter_output', formatted);
-        }
-        else {
-            appendChildNodes("interpreter_output",
-                SPAN({"class": "data"}, res),
-                BR()
-            );
-        }
-
-        this.doScroll();
-    }
-};
-
 window.writeln = function () {
     appendChildNodes("interpreter_output",
         SPAN({"class": "data"}, arguments),
@@ -255,68 +129,8 @@ window.clear = function () {
     getElement("interpreter_area").scrollTop = 0;
 };
 
-window.blockOn = function (d) {
-    if (!(d instanceof Deferred)) {
-        throw new TypeError(repr(d) + " is not a Deferred!");
-    }
-    interpreterManager.blockOn(d);
-};
-
-window.dir = function (o) {
-    // Python muscle memory!
-    return sorted(keys(o));
-};
-
-window.inspect = function (o) {
-    window._ = o;
-    if ((typeof(o) != "function" && typeof(o) != "object") || o == null) {
-        window.writeln(repr(o));
-        return;
-    }
-    var pairs = items(o);
-    if (pairs.length == 0) {
-        window.writeln(repr(o));
-        return;
-    }
-    window.writeln(TABLE({"border": "1"},
-        THEAD({"class": "invisible"}, TR(null, TD(), TD())),
-        TFOOT({"class": "invisible"}, TR(null, TD(), TD())),
-        TBODY(null,
-            map(
-                function (kv) {
-                    var click = function () {
-                        try {
-                            window.inspect(kv[1]);
-                        } catch (e) {
-                            interpreterManager.showError(e);
-                        }
-                        return false;
-                    }
-                    return TR(null,
-                        TD(null, A({href: "#", onclick: click}, kv[0])),
-                        TD(null, repr(kv[1]))
-                    );
-                },
-                pairs
-            )
-        )
-    ));
-};
-    
 interpreterManager = new InterpreterManager();
 addLoadEvent(interpreterManager.initialize);
-
-// rewrite the view-source links
-addLoadEvent(function () {
-    var elems = getElementsByTagAndClassName("A", "view-source");
-    var page = "interpreter/";
-    for (var i = 0; i < elems.length; i++) {
-        var elem = elems[i];
-        var href = elem.href.split(/\//).pop();
-        elem.target = "_blank";
-        elem.href = "../view-source/view-source.html#" + page + href;
-    }
-});
 
 InterpreterManager.prototype.setTeamwork = function (e) {
     /* Handle the various teamwork settings. */
