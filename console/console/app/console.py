9c2e9b02008b6fc1ee4d55e0ee25c7ab8570ee43
Jason Smith <jhs@proven-corporation.com>
2009-01-21 18:05:04 +0700

    Update copyright notices for the new year

--- console/console/app/console.py
+++ console/console/app/console.py
@@ -1,6 +1,6 @@
 # The WSGI entry-point for App Engine Console
 #
-# Copyright 2008 Proven Corporation Co., Ltd., Thailand
+# Copyright 2008-2009 Proven Corporation Co., Ltd., Thailand
 #
 # This file is part of App Engine Console.
 #
